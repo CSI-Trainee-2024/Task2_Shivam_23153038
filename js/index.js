8517d19a16fa85e18c1bed4e1b42d9c01446251e
shivam gupta <sgg86953@gmail.com>
2024-10-12 22:32:47 +0530

    Added JavaScript functionality for deleting workout items

--- js/index.js
+++ js/index.js
@@ -19,11 +19,29 @@ function showOnPage(){
     const item = "";
     for(let i=0;i<workouts.length;i++){
         item += ` <li class = "col-1">
-        <div class = "exercisename"> ${workouts[i].name}--X ${workouts[i].sets} </div>
+                        <div class = "exercisename"> ${workouts[i].name}--X ${workouts[i].sets} </div>
         <div class= "col-2"
         
         ${convertTime(workouts[i].duration)} </div>
-        <button class = "col-4" data-index ="${i}"> delete </button>`
+        <button class = "col-4 deleteButton" data-index ="${i}"> delete </button>
+        </li>`;
     }
+document.getElementsByClassName("table").innerHtml = item;
+showOnPage();
+}
 
+function deletelist(){
+    let deleteButton = document.querySelectorAll (".deleteButton");
+
+    deleteButton.forEach((btn)=>{
+        btn.addEventListener("click",()=>{
+            let index = btn.getAttribute("data-index");
+            deleteItem(index);
+        });
+    });
+}
+function deleteItem(index){
+    workouts.splice(index,1);
+    localStorage.setItem("workouts",JSON.stringify(workouts));
+    showOnPage();
 }
